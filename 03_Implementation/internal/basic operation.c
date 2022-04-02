77bc0d34436a16eea5a71bb1fff903097aa7d26c
Sowmiya <101380469+sowmiya1807@users.noreply.github.com>
2022-04-03 02:17:45 +0530

    Add files via upload

---
+++ 03_Implementation/internal/basic operation.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+
+int addition(int a, int b)
+{
+ return (a+b); //returning  result to main funtion 
+}
+
+int subtraction(int a, int b)
+{ 
+    return (a-b); //returning  result to main funtion 
+} 
+
+int multiplication(int a,int b)
+{
+    return (a*b);    //returning  result to main funtion 
+}
+
+int division(int a,int b)
+{
+    return (a/b);    //returning  result to main funtion 
+}
